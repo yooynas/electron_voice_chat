f1c454595371abe7f12fcbce3db0a27208f566d2
bcchoi <bcc@isans.co.kr>
2016-09-09 17:37:34 +0900

    test complete

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 var exec = require('child_process').exec;
 exec('node server.js', function(err, stdout, stderr){
   if (err) { console.log(err); }
 });
 
-let mainWindow
+var mainWindow = null;
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 320, height: 480})
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow = new BrowserWindow({width: 320, height: 480});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.openDevTools();
   mainWindow.on('closed', function () {
     mainWindow = null
   })
 }
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
